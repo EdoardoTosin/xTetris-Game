174777f793b01cf1ebda98dffa66f32679452b1c
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2021-10-14 17:18:44 +0200

    Update xTetris.c

--- src/xTetris.c
+++ src/xTetris.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <wchar.h>
 #include <locale.h>
+#include <stdlib.h>
 #include "definitions.h"
 #include "common.h"
 
@@ -9,14 +9,22 @@
    \brief Main entry point of the program.
 */
 int main (void) {
+  setlocale(LC_CTYPE, "");
   system(CLEAR);
+  /*
   puts("/====================/");
   puts("        xTetris       ");
   puts("/====================/");
-  setlocale(LC_CTYPE, "");
-  wchar_t ch = 0x254u;
-  wprintf(L"%lc\n", ch);
-
+  */
+  wchar_t ch = 0x1f533;
+  int i, j;
+  for (i=0; i<10; i++){
+    for (j=0; j<10; j++){
+      wprintf(L"%lc", ch);
+    }
+    wprintf(L"\n");
+  }
+  wprintf(L"Test success.\n");
   delayTimer(1);
   return 0;
 }
