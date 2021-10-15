4417b68ced5111fdb4a62b0d09a4be8896731428
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2021-10-15 18:49:08 +0200

    edit comment

--- src/initBoard.h
+++ src/initBoard.h
@@ -2,6 +2,6 @@
 
 /*!
    \brief Create the board.
-   @param[out] mat Struct empty at the start to contain the dropping pieces during gameplay.
+   @param[out] mat Struct empty at the start for the tetriminos cells.
 */
 BoardPtr initialize();
