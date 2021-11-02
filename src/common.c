b7fbd1b740e88ddff5394ff88c85af3cd3308e9a
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2021-11-02 13:28:33 +0100

    clearCLI implementation

--- src/common.c
+++ src/common.c
@@ -1,6 +1,14 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <wchar.h>
 #include "definitions.h"
+
+void clearCLI(){
+  int result;
+  result = system(CLEAR);
+  if (result == 1)
+    exit(EXIT_FAILURE);
+}
 
 void delayTimer(int timeDelay) {
   #ifdef _WIN32
@@ -10,5 +18,5 @@ void delayTimer(int timeDelay) {
   #endif
 }
 
-wchar_t eSq = 0x2B1C; // emptySquare
-wchar_t fSq = 0x2B1B; // fullSquare
+wchar_t eSq = 0x2B1C; /* emptySquare */
+wchar_t fSq = 0x2B1B; /* fullSquare */
