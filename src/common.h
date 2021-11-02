0d2ec7f6c3385af7cd1d7c68e7089f804db7cb67
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2021-11-02 13:28:33 +0100

    clearCLI implementation

--- src/common.h
+++ src/common.h
@@ -1,4 +1,8 @@
 /*!
+   \brief Call the system function to clear the cli.
+*/
+void clearCLI();
+/*!
    \brief Call the system sleep with closs-platform compatibility.
    @param timeDelay Time of the sleep.
 */
@@ -7,8 +11,8 @@ void delayTimer(int timeDelay);
 /*!
    \brief Assign unicode character (white square) to this global variable.
 */
-wchar_t eSq; // emptySquare
+wchar_t eSq;
 /*!
    \brief Assign unicode character (black square) to this global variable.
 */
-wchar_t fSq; // fullSquare
+wchar_t fSq;
