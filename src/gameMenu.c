5ffe6942a6b7fcc150fbce90062403f85f5cfdbd
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2022-01-13 00:25:12 +0100

    gameMenu functions implementation

--- src/gameMenu.c
+++ src/gameMenu.c
@@ -2,33 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <wchar.h>
 
 #include "common.h"
 #include "definitions.h"
 
 void printMainMenu(int opt){
-  clearCLI();
-  wprintf(L"\a");
-  if(opt==1){
+  if(opt==2){
     wprintf(L"-->Start Game<--\r\n");
     wprintf(L"      Rules\r\n");
     wprintf(L"     Credits\r\n");
     wprintf(L"      Exit\r\n");
   }
-  else if(opt==2){
+  else if(opt==3){
     wprintf(L"   Start Game\r\n");
     wprintf(L"   -->Rules<--\r\n");
     wprintf(L"     Credits\r\n");
     wprintf(L"      Exit\r\n");
   }
-  else if(opt==3){
+  else if(opt==4){
     wprintf(L"   Start Game\r\n");
     wprintf(L"      Rules\r\n");
     wprintf(L"  -->Credits<--\r\n");
     wprintf(L"      Exit\r\n");
   }
-  else if(opt==4){
+  else if(opt==5){
     wprintf(L"   Start Game\r\n");
     wprintf(L"      Rules\r\n");
     wprintf(L"     Credits\r\n");
@@ -36,32 +33,77 @@ void printMainMenu(int opt){
   }
 }
 
-int crtlMainMenu(){
-  int key, opt;
-  printMainMenu(opt=1);
+void playersName(wchar_t* pl1, wchar_t* pl2){
+  do{
+    wprintf(L"Enter Player 1 name:\r\n");
+    wscanf(L"%ls", pl1);
+  }
+  while(pl1!=L"\0");
+  do{
+    wprintf(L"Enter Player 1 name:\r\n");
+    wscanf(L"%ls", pl2);
+  }
+  while(pl2!=L"\0");
+}
+
+void printGuide(){
+  wprintf(L"- Goal: Put your organizational skills and endurance to the test by clearing as many lines as possible.\r\n");
+  wprintf(L"- Clear lines: Maneuver the falling Tetriminos to fit them together within the Matrix. To clear a line, fill every square within a single row.\r\n");
+  wprintf(L"- Score points: Earn points by clearing lines. Clear multiple lines at once to increase your scoring opportunities.\r\n");
+  wprintf(L"- Ghost piece: Use the Ghost Piece to determine the best fit for the falling Tetrimino. This helpful guide appears directly below the falling Tetrimino and displays possible placements.\r\n");
+  wprintf(L"- Next queue: Preview the upcoming Tetrimino in the Next Queue to plan ahead and increase your scoring opportunities.\r\n");
+  wprintf(L"- Hold queue: Store a falling Tetrimino in the Hold Queue for later use.\r\n");
+  wprintf(L"- Game over: Stack the Tetriminos too high and the game is over!\r\n");
+  wprintf(L"\r\n");
+  wprintf(L"\r\n");
+  wprintf(L"   -->Return<--\r\n");
+}
+
+void printCredits(){
+  wprintf(L"Developer:\r\n");
+  wprintf(L" Edoardo Tosin\r\n");
+  wprintf(L"\r\n");
+  wprintf(L"\r\n");
+  wprintf(L"   -->Return<--\r\n");
+}
+
+void printCurrentMenu(int menu, int opt){
+  clearCLI();
+  wprintf(L"\a");
+  if(menu==1)
+    printMainMenu(opt);
+  else if(menu==3)
+    printGuide();
+  else if(menu==4)
+    printCredits();
+}
+
+int choiceCtrl(int menu, int opt, int min, int max){
+  int key;
+  printCurrentMenu(menu, opt);
   while(1){
     if(u_kbhit()){
       key=u_getchar();
-      if(key==119 || key==87){
-        opt--;
-        if(opt<1)
-          opt=4;
-        printMainMenu(opt);
-      }
-      else if(key==115 || key==83){
-        opt++;
-        if(opt>4)
-          opt=1;
-        printMainMenu(opt);
+      if(key==119 || key==87 || key==115 || key==83){
+        if(key==119 || key==87){
+          opt--;
+          if(opt<min)
+            opt=max;
+        }
+        else if(key==115 || key==83){
+          opt++;
+          if(opt>max)
+            opt=min;
+        }
+        printCurrentMenu(menu, opt);
       }
       else if(key==13){
-        return 1;
+        return opt;
       }
       else if(key==27)
         return 0;
     }
-    usleep(100);
+    /*usleep(100);*/
   }
-
   return 0;
 }
