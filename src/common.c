703bdb0dc58f64b521dfff26096885d7e8169975
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2022-01-12 13:13:18 +0100

    asynchronous com implementation

--- src/common.c
+++ src/common.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <wchar.h>
+
+#include <termios.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
 #include "definitions.h"
 
 void clearCLI(){
@@ -10,7 +16,7 @@ void clearCLI(){
     exit(EXIT_FAILURE);
 }
 
-void delayTimer(int timeDelay) {
+void delayTimer(int timeDelay){
   #ifdef _WIN32
   Sleep(timeDelay);
   #else
@@ -20,3 +26,48 @@ void delayTimer(int timeDelay) {
 
 wchar_t eSq = 0x2B1C; /* emptySquare */
 wchar_t fSq = 0x2B1B; /* fullSquare */
+
+static struct termios orig_term;
+
+void u_cleanup(void){
+  tcsetattr(0, TCSANOW, &orig_term);
+}
+
+int u_kbhit(void){
+  struct termios t;
+  int ret;
+  fd_set rfd;
+  struct timeval to;
+  static int first_hit=0;
+
+  if(first_hit==0){
+    if(tcgetattr(0, &t)!=0)
+      exit(0);
+    orig_term=t;
+    cfmakeraw(&t);
+    if(tcsetattr(0, TCSANOW, &t)!=0)
+      exit(0);
+    atexit(u_cleanup);
+    first_hit=1;
+  }
+
+  FD_ZERO(&rfd);
+  FD_SET(0, &rfd);
+  to.tv_sec=0;
+  to.tv_usec=0;
+  if(select(1, &rfd, NULL, NULL, &to)==1)
+    return 1;
+  return 0;
+}
+
+int u_getchar(void){
+  int ret;
+  fd_set rfc;
+  unsigned char buf;
+
+  if(read(0, &buf, 1)!=1)
+    ret=0;
+  else
+    ret=buf;
+  return ret;
+}
