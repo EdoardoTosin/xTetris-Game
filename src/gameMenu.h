0814a37100d07214a0def82d5e8705c4d031e150
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2022-01-13 00:25:12 +0100

    gameMenu functions implementation

--- src/gameMenu.h
+++ src/gameMenu.h
@@ -1,3 +1,40 @@
+#include <wchar.h>
+
+/*!
+   \brief Print main menu.
+   @param[in] opt Define the selected option in the menu at that moment.
+*/
 void printMainMenu(int opt);
 
-int mainMenu();
+/*!
+   \brief Prepare the gameplay asking for player names.
+   @param[in] pl1 Pointer to Player 1 name.
+   @param[in] pl2 Pointer to Player 2 name.
+*/
+void playersName(wchar_t* pl1, wchar_t* pl2);
+
+/*!
+   \brief Print the rules menu.
+*/
+void printGuide();
+
+/*!
+   \brief Print the credits menu.
+*/
+void printCredits();
+
+/*!
+   \brief Call the print of the current menu.
+   @param[in] menu Define the current menu.
+   @param[in] opt Define the current option selected.
+*/
+void printCurrentMenu(int menu, int opt);
+
+/*!
+   \brief Create the board.
+   @param[in] menu Define the current menu.
+   @param[in] opt Define the current option selected.
+   @param[in] min Lowest value of option in current menu.
+   @param[in] max Highest value of option in current menu.
+*/
+int choiceCtrl(int menu, int opt, int min, int max);
