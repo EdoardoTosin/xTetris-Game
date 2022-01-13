9fd7b399fd65b606f8d2a0741cf02df1bb6cdd5e
EdoardoTosin <7118135+EdoardoTosin@users.noreply.github.com>
2022-01-13 17:29:25 +0100

    Update xTetris.c

--- src/xTetris.c
+++ src/xTetris.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
+#include <unistd.h> /* sleep function */
+
+
 #include "common.h"
+#include "definitions.h"
 #include "printGame.h"
+#include "gameMenu.h"
 
 #include "struct.h"
 #include "initBoard.h"
@@ -10,19 +15,51 @@
 /*!
    \brief Main entry point of the program.
 */
-int main (void) {
+int main (int argc, char *argv[]) {
+  int nChoices[2][4];
+  int menu, opt, key=2;
+
   BoardPtr board_1, board_2;
   wchar_t* pl1;
   wchar_t* pl2;
-  setlocale(LC_CTYPE, "");
-  printIntro();
-  pl1 = L"Player AAAAA";
-  pl2 = L"Player BBBBB";
+
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+  pl1 = L"\0";
+  pl2 = L"\0";
   board_1 = board_2 = initialize();
-  printBoard(board_1, board_2, pl1, pl2);
-  /*
-  delayTimer(1);
-  */
+
+  setlocale(LC_CTYPE, "");
+  do{
+    printIntro();
+    key = waitUserInput();
+  }
+  while(key!=ESCAPE && key!=CARRIAGE_RETURN);
+
+  nChoices[0][0]=2;
+  nChoices[1][0]=5;
+  nChoices[0][1]=nChoices[1][1]=6;
+  nChoices[0][2]=nChoices[1][2]=7;
+  nChoices[0][3]=nChoices[1][3]=8;
+  menu=1;
+  opt=nChoices[0][menu-1];
+  do{
+    if(menu!=2){ /* 2->Launch Gameplay */
+      if(opt==6 || opt==7){
+        menu=1;
+        opt=opt-4;
+      }
+      opt = choiceCtrl(menu, opt, nChoices[0][menu-1], nChoices[1][menu-1]);
+    }
+    else{
+      playersName(pl1, pl2);
+      printBoard(board_1, board_2, pl1, pl2);
+      opt=5;
+    }
+    menu=opt;
+  }
+  while(opt!=5);
+  clearCLI();
   return 0;
 }
 
