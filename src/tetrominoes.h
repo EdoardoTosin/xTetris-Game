cf46d1ed35e31cc461bf12d0a8d9902139a951fc
Edoardo Tosin <7118135+EdoardoTosin@users.noreply.github.com>
2022-04-13 14:50:09 +0200

    Reorganize tetromino files and struct

--- src/tetrominoes.h
+++ src/tetrominoes.h
@@ -1,13 +1,30 @@
 #ifndef TETROMINOES_H
 #define TETROMINOES_H
 
-#include "struct.h"
+#include <stdlib.h>
 
-/* Rappresentazione di un tetramino */
-struct s_tetromino {
-	int container[4][4];
-	int exists;
-};
-typedef struct s_tetromino* t_tetromino;
+#include "struct.h"
+#include "definitions.h"
+#include "tetrominoes.h"
+
+/*!
+   \brief Fill a single cell inside tetromino matrix.
+   @param[in] t Struct tetromino.
+	 @param[in] i Row of the cell to fill.
+	 @param[in] j Column of the cell to fill.
+*/
+void fill(TetrominoPtr* t, int i, int j);
+
+/*!
+   \brief Fill all tetromino matrix with zeros.
+   @param[in] t Struct tetromino.
+*/
+void iniTetrominoPtr(TetrominoPtr* t);
+
+/*!
+   \brief Initialize and allocate all tetrominoes.
+   @param[in] tetromat Struct tetromino.
+*/
+void init_tetromat(TetrominoPtr tetromat[NPIECES][TETROMINO_ROT]);
 
 #endif
